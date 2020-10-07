16989e5f413fac0c2f670240efc6f69cd0d2050d
Ryan Blackman <rblackman24@gmail.com>
2020-10-07 14:21:25 -0500

    move to desktop

--- WorkerService/Program.cs
+++ WorkerService/Program.cs
@@ -24,7 +24,7 @@ namespace KioskAnnunciatorButton.WorkerService
 			}
 			catch (Exception ex)
 			{
-				File.WriteAllText(Path.Combine(Assembly.GetEntryAssembly().Location, "CrashLog.txt"), $"Service failed to start:\n\n{ex.Message}");
+				File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "CrashLog.txt"), $"Service failed to start:\n\n{ex.Message}");
 				Log.Logger.Fatal(ex, "Application unable to start");
 				throw ex;
 			}
