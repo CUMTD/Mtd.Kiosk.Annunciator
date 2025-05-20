eac88043e347072f78e1408b55a08c9c31b43a02
Matthew Novelli <mattnovelli@gmail.com>
2025-05-20 13:34:14 -0500

    pi reader fixes

--- Readers/Raspi/Config/PiReaderConfig.cs
+++ Readers/Raspi/Config/PiReaderConfig.cs
@@ -6,7 +6,10 @@ public class PiReaderConfig
 	public const string ConfigSectionName = "PiReader";
 
 	[Required]
-	public required int[] Pins { get; set; }
+	public required int Pin { get; set; }
 	public bool Enabled { get; set; }
+
+	[Required]
+	public bool ExternalResistor { get; set; }
 
 }
